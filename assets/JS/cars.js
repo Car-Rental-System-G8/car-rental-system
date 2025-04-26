20fe028080ae002e2f4d3c48aa1036a3ef4420e7
mvicsa <mvicsa@yahoo.com>
2025-04-26 17:08:51 +0300

    Update Cars Functions

--- assets/JS/cars.js
+++ assets/JS/cars.js
@@ -1,4 +1,4 @@
-import { addCarForm, displayCars, getCars } from "./modules/handleCars.js";
+import { addCarForm, displayCars, getCars } from "./modules/carManager.js";
 
 window.addEventListener("load", async () => {
   addCarForm();
